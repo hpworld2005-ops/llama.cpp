af50ba80564fc004c452348f38732100d3c20c9c
hpworld2005-ops <hpworld2005@gmail.com>
2026-08-30 00:53:28 +0530

    Update ServerService.java

--- app/src/main/java/com/naina/ai/ServerService.java
+++ app/src/main/java/com/naina/ai/ServerService.java
@@ -32,7 +32,8 @@ public class ServerService extends Service {
         if (proc == null) {
             try {
                 String bin = getApplicationInfo().nativeLibraryDir + "/libllamaserver.so";
-                String model = "/sdcard/Download/Naina.gguf";
+                String model = getFilesDir().getAbsolutePath() + "/naina.gguf";
+
 
                 ProcessBuilder pb = new ProcessBuilder(
                         bin, "-m", model,
